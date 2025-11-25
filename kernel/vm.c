3f59213c10ff534dc19d5459376277d07a3739b6
trikesh <trikesh12344321@gmail.com>
2025-11-25 22:52:13 +0300

    notworkin part4

--- kernel/vm.c
+++ kernel/vm.c
@@ -323,12 +323,17 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+    ref_inc((void*)pa);
     flags = PTE_FLAGS(*pte);
     flags &= ~PTE_W;
+    *pte &= ~PTE_W;
+    *pte |= PTE_COW;
+    flags |= PTE_COW;
     // if((mem = kalloc()) == 0)
     //   goto err;
     // memmove(mem, (char*)pa, PGSIZE);
     if(mappages(new, i, PGSIZE, pa, flags) != 0){
+      ref_dec((void*)pa);
       // kfree(mem);
       goto err;
     }
@@ -364,16 +369,23 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
-    if(va0 >= MAXVA)
-      return -1;
     pte = walk(pagetable, va0, 0);
-    if(pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0 ||
-       (*pte & PTE_W) == 0)
+    if(pte == 0 || (*pte & PTE_V) == 0)
       return -1;
+    if((*pte & PTE_COW) && (*pte & PTE_W) == 0) {
+      if(uvmcow(pagetable, va0) < 0)
+        return -1;
+      pte = walk(pagetable, va0, 0);
+    }
+    
     pa0 = PTE2PA(*pte);
+    if(pa0 == 0)
+      return -1;
+    
     n = PGSIZE - (dstva - va0);
     if(n > len)
       n = len;
+    
     memmove((void *)(pa0 + (dstva - va0)), src, n);
 
     len -= n;
@@ -477,4 +489,39 @@ void vmprint(pagetable_t pt)
 {
   printf("page table %p\n", pt);
   _vmprint(pt, 0);
+}
+
+int
+uvmcow(pagetable_t pagetable, uint64 va)
+{
+  pte_t *pte;
+  uint64 pa;
+  char *mem;
+  
+  va = PGROUNDDOWN(va);
+  pte = walk(pagetable, va, 0);
+  if(pte == 0) 
+    return -1;
+  if((*pte & PTE_V) == 0) 
+    return -1;
+  if(!(*pte & PTE_COW))
+    return -1;
+  pa = PTE2PA(*pte);
+  if(ref_get((void*)pa) == 1) {
+    *pte |= PTE_W;
+    *pte &= ~PTE_COW;
+    return 0;
+  }
+  if((mem = kalloc()) == 0){
+    return -1;
+  }
+  memmove(mem, (char*)pa, PGSIZE);
+  ref_dec((void*)pa);
+  uint64 flags = (PTE_FLAGS(*pte) | PTE_W) & ~PTE_COW;
+  uvmunmap(pagetable, va, 1, 0);
+  if(mappages(pagetable, va, PGSIZE, (uint64)mem, flags) != 0) {
+    kfree(mem);
+    return -1;
+  }
+  return 0;
 }
