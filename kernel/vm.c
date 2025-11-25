80c014a667b6aab2a20761d63f5b596d585efd5f
trikesh <trikesh12344321@gmail.com>
2025-11-25 16:58:26 +0300

    part 2 vmprint added

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void _vmprint(pagetable_t pt, int level){
+  for (int i=0; i<512; ++i){
+    pte_t pte = pt[i];
+    if((pte & PTE_V)){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      switch (level){
+        case 2:
+          printf(".. ");
+        case 1:
+          printf(".. ");
+        case 0:
+          printf("..");
+          break;
+      }
+      printf("%d: pte %p pa %p\n", i, (void*)pte, (void*)child);
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0)
+        _vmprint((pagetable_t)child, level + 1);
+    } 
+  }
+}
+
+void vmprint(pagetable_t pt)
+{
+  printf("page table %p\n", pt);
+  _vmprint(pt, 0);
+}
