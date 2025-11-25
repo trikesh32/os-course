9ec69b87d5889986e7a6d9bd24ed983b38d40d3b
trikesh <trikesh12344321@gmail.com>
2025-11-25 17:33:23 +0300

    part3 done

--- kernel/vm.c
+++ kernel/vm.c
@@ -324,6 +324,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
+    flags &= ~PTE_W;
     // if((mem = kalloc()) == 0)
     //   goto err;
     // memmove(mem, (char*)pa, PGSIZE);
